3d1bc9e51b69d72161e3d123579faed9c93acad6
Pc <filipgolovicc@gmail.com>
2024-09-04 09:14:47 +0200

    Add first views

---
+++ unitor/unitor/views.py
@@ -0,0 +1,27 @@
+from rest_framework import viewsets
+from .models import Korisnik, UposljenaJedinica, PomocniRadnici, Lokacije, EksterniOglasivaci, Steta
+from .serializers import KorisnikSerializer, UposljenaJedinicaSerializer, PomocniRadniciSerializer, LokacijeSerializer, EksterniOglasivaciSerializer, StetaSerializer
+
+class KorisnikViewSet(viewsets.ModelViewSet):
+    queryset = Korisnik.objects.all()
+    serializer_class = KorisnikSerializer
+
+class UposljenaJedinicaViewSet(viewsets.ModelViewSet):
+    queryset = UposljenaJedinica.objects.all()
+    serializer_class = UposljenaJedinicaSerializer
+
+class PomocniRadniciViewSet(viewsets.ModelViewSet):
+    queryset = PomocniRadnici.objects.all()
+    serializer_class = PomocniRadniciSerializer
+
+class LokacijeViewSet(viewsets.ModelViewSet):
+    queryset = Lokacije.objects.all()
+    serializer_class = LokacijeSerializer
+
+class EksterniOglasivaciViewSet(viewsets.ModelViewSet):
+    queryset = EksterniOglasivaci.objects.all()
+    serializer_class = EksterniOglasivaciSerializer
+
+class StetaViewSet(viewsets.ModelViewSet):
+    queryset = Steta.objects.all()
+    serializer_class = StetaSerializer
