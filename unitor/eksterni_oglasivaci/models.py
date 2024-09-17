a2c7bfebf5b8d20a26d3a727399a32fadad10a77
Pc <filipgolovicc@gmail.com>
2024-09-17 21:45:46 +0200

    Add authentication and authorization

--- unitor/eksterni_oglasivaci/models.py
+++ unitor/eksterni_oglasivaci/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from unitor.unitor.models import Korisnik
+
+
 class EksterniOglasivaci(models.Model):
     unique_id = models.AutoField(primary_key=True)
     korisnik = models.ForeignKey(Korisnik, related_name='eksterni_oglasivaci', on_delete=models.CASCADE)
