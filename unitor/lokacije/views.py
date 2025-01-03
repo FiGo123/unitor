3c897c0875853f44cc096638067e2c5db204abc0
Pc <filipgolovicc@gmail.com>
2025-01-03 23:22:28 +0100

    Add post and put method in Lokacije view

--- unitor/lokacije/views.py
+++ unitor/lokacije/views.py
@@ -5,3 +5,13 @@ from serializers import LokacijeSerializer
 class LokacijeViewSet(viewsets.ModelViewSet):
     queryset = Lokacije.objects.all()
     serializer_class = LokacijeSerializer
+
+    # Create (POST) method
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return response
+
+    # Update (PUT) method
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        return response
