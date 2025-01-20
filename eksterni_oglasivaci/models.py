be20e2ac36307c76df7b47a8ba954cc111487b6b
Pc <filipgolovicc@gmail.com>
2025-01-20 21:56:11 +0100

    Add platform and api key models for extern platforms

--- eksterni_oglasivaci/models.py
+++ eksterni_oglasivaci/models.py
@@ -3,7 +3,24 @@ from django.db import models
 from unitor.models import Korisnik
 
 
+class Platforma(models.Model):
+    name = models.CharField(max_length=100, unique=True)  # npr. "Airbnb", "Booking.com"
+
+    def __str__(self):
+        return self.name
+
 class EksterniOglasivaci(models.Model):
     unique_id = models.AutoField(primary_key=True)
     korisnik = models.ForeignKey(Korisnik, related_name='eksterni_oglasivaci', on_delete=models.CASCADE)
-    # Add other fields as required
+
+    platform = models.ForeignKey(Platforma, related_name='eksterni_oglasivaci', on_delete=models.CASCADE)
+    api_key = models.CharField(max_length=255)
+    last_sync = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('korisnik', 'platform')  # Svaki korisnik može imati jedan ključ po platformi
+        verbose_name = "Eksterni Oglasivač"
+        verbose_name_plural = "Eksterni Oglasivači"
+
+    def __str__(self):
+        return f"{self.korisnik} - {self.platform}"
